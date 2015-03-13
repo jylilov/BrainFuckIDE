bd2b598aec399714f602fe1d8304a45749fbb4b7
Alexander Jylilov <alexander.jylilov@gmail.com>
2015-03-13 21:51:10 +0300

    Created MainWindow class

---
+++ src/main/java/by/jylilov/brainfuckide/MainWindow.java
@@ -0,0 +1,11 @@
+package by.jylilov.brainfuckide;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+        super("BrainFuck IDE");
+    }
+
+}
--- src/main/java/by/jylilov/brainfuckide/Start.java
+++ src/main/java/by/jylilov/brainfuckide/Start.java
@@ -3,11 +3,16 @@ package by.jylilov.brainfuckide;
 import javax.swing.*;
 
 public class Start {
+
+    private static final int DEFAULT_WIDTH = 800;
+    private static final int DEFAULT_HEIGHT = 600;
+
     public static void main(String [] args) {
-        JFrame frame = new JFrame("BrainFuck IDE");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        MainWindow window = new MainWindow();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
+
 }
